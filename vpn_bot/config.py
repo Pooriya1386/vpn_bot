c2a97110b8babcc22d1af4af94bbb81486f4bbc1
Pooriya1386 <141553808+Pooriya1386@users.noreply.github.com>
2025-07-26 12:14:43 +0330

    config.py

--- vpn_bot/config.py
+++ vpn_bot/config.py
@@ -1,10 +1,6 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = os.getenv("ADMIN_ID")
 HIDDIFY_API_KEY = os.getenv("HIDDIFY_API_KEY")
 HIDDIFY_PANEL_URL = os.getenv("HIDDIFY_PANEL_URL")
-
-CARD_NUMBER = os.getenv("CARD_NUMBER")
-CARD_NAME = os.getenv("CARD_NAME")
-SUPPORT_ID = os.getenv("SUPPORT_ID")
+ADMIN_ID = int(os.getenv("ADMIN_ID", 123456789))  # آیدی عددی ادمین
